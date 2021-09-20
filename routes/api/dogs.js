74482773e703ec82ae6dd6d3f76667b52c1b7e49
alexsaintlam <alexlam@DESKTOP-ILBQ44R.localdomain>
2021-09-20 12:15:08 -0400

    dog update

--- routes/api/dogs.js
+++ routes/api/dogs.js
@@ -68,6 +68,38 @@ passport.authenticate('jwt', { session: false }),
     res.status(404).json({ nodogsfound: 'No dog found' }))
 });
 
+router.patch('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateDogInput(req.body);
+  
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+    
+      const updatedDog = {
+        name: req.body.name,
+        gender: req.body.gender,
+        age: req.body.age,
+        breed: req.body.breed
+      }
+
+      Dog.findById(req.params.id)
+      .then(dog => {
+          if (dog.user.toHexString() !== req.user.id) {
+            res.status(404).json({ notauthorized: 'This is not your dog'})
+          } else {
+            Dog.findOneAndUpdate({ _id: req.params.id }, 
+              {$set:updatedDog}, {new: true})
+                .then(returnedDog => res.json(returnedDog))
+                .catch(() =>
+                  res.status(404).json("unable to update"))
+          }
+      })
+      .catch(() =>
+        res.status(404).json({ nodogfound: 'No dog found' }))  
+  })
+
 
 
   module.exports = router;
