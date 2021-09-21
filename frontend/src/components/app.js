2d5b45e23d1742a81f429f3f5fd6abcecd20ff82
Winnie <winnieng3210@gmail.com>
2021-09-21 13:59:32 -0400

    ready to merge with main

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 // import { ProtectedRoute } from "../util/route_util";
 import Modal from "./modal/modal";
-// import Splash from "./splash/splash_container";
+import Splash from "./splash/splash_container";
 // import LogIn from "./session/login_form_container";
 // import Signup from "./session/signup_form_container";
 import DogIndexContainer from "../components/dog/dog_index_container";
@@ -15,13 +15,13 @@ import HeaderContainer from "../components/header/header_container";
 const App = () => (
   <div>
     <Modal />
-    <Splash />
+    {/* <Splash /> */}
     {/* <DogIndexContainer /> */}
     <HeaderContainer />
-    <Route exact path="/dogs" component={DogIndexContainer} />
     <Route exact path="/dogs/:id" component={DogShowContainer} />
     <Route exact path="/dogs/new" component={DogCreateContainer} />
     <Route exact path="/dogs/update/:id" component={DogEditContainer} />
+    <Route exact path="/dogs" component={DogIndexContainer} />
     <Route exact path="/" component={Splash} />
     <Route exact path="/users/:id" component={UserShowContainer} />
   </div>
