d5c261abb796e462b875bf34e500a753271db946
alexsaintlam <alexlam@DESKTOP-ILBQ44R.localdomain>
2021-09-22 23:55:08 -0400

    socket io

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const User = require('./models/User');
 const dogs = require("./routes/api/dogs");
 const bodyParser = require('body-parser');
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: "*" }});
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./server_socketio/users_util');
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
@@ -34,7 +38,34 @@ app.use(express.urlencoded({
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+server.listen(8080, () => {console.log('Server is running on 8080')});
 
 app.use("/api/users", users);
 app.use("/api/dogs", dogs);
 
+io.on('connection', socket => {
+  socket.on('join', ({name, room}, callback) => {
+    const {error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback('error');
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`}); // send message to user
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`}); // send message to everyone in the room
+
+    socket.join(user.room); // add user to the room
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message}); // listens for message from user and sends message to room??
+
+    callback();
+  })
+
+  socket.on('disconnnect', () => {
+    console.log('user has left')
+  })
+})
