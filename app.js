fac3343dadcb71b73a06073065deb9de341317c8
julian <juka1031@gmail.com>
2021-09-23 20:34:06 -0400

    pre pushing to heroku

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(
 );
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
 server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
